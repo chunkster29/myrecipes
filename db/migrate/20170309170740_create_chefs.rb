4c5158515f5ea05dc52994f9d319f4abdb596500
Jason Turner <chunkster29@gmail.com>
2017-03-15 20:30:27 +0000

    implement edit and delete

--- db/migrate/20170309170740_create_chefs.rb
+++ db/migrate/20170309170740_create_chefs.rb
@@ -2,7 +2,7 @@ class CreateChefs < ActiveRecord::Migration[5.0]
   def change
     create_table :chefs do |t|
       t.string :chefname
-      t.string :email
+      t.string :description
       t.timestamps
     end
   end
