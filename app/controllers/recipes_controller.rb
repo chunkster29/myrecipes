71f1943446448101feb041fb3d62222cc98b3b5a
Jason Turner <chunkster29@gmail.com>
2017-03-15 20:23:32 +0000

    implement edit and delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,10 +16,30 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(recipe_params)
     @recipe.chef = Chef.first
     if @recipe.save
-      flash[:success] = "recipe was created successfully!"
+      flash[:success] = "Recipe was created successfully!"
       redirect_to recipe_path(@recipe)
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+    
+    def destroy
+      Recipe.find(params[:id]).destroy
+      flash[:success] = "Recipe deleted successfully"
+      redirect_to recipes_path
     end
   end
   
