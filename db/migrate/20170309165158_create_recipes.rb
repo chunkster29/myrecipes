903e4840ef65a4b14677f2e4e369842bb3c6f51e
Jason Turner <chunkster29@gmail.com>
2017-03-15 20:35:13 +0000

    implement edit and delete1

--- db/migrate/20170309165158_create_recipes.rb
+++ db/migrate/20170309165158_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[5.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.string :description
+      t.string :email
       t.timestamps
     end
   end
